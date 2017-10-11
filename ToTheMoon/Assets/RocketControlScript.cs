1f2f24cb9eeff2cd08eb4cdb0f2dcd7f6a036ac0
Borstentorsten <paule-mail@gmx.net>
2017-10-11 21:59:28 +0200

    Rocketrotation dampering

--- ToTheMoon/Assets/RocketControlScript.cs
+++ ToTheMoon/Assets/RocketControlScript.cs
@@ -11,12 +11,13 @@ public class RocketControlScript : MonoBehaviour {
     }
 
     public SideEnum side;
-    public ParticleSystem particleSystem;
+    public ParticleSystem ParticleSystem;
 
     Rigidbody rigidBody;
     float forceMultiplier = 1.0f;
     float maxForceMultiplier = 8f;
     float minForceMultiplier = 1.0f;
+    float maxRotation = 60;
 	// Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
@@ -29,17 +30,54 @@ public class RocketControlScript : MonoBehaviour {
     private void FixedUpdate()
     {
         KeyCode powerKey = KeyCode.Space;
-        if (side == SideEnum.Left)
-            powerKey = KeyCode.LeftArrow;
-        else if (side == SideEnum.Right)
-            powerKey = KeyCode.RightArrow;
 
-        if (Input.GetKey(powerKey))
+        bool canAddMoreForce = true;
+        float rotation = transform.rotation.eulerAngles.z;
+        if (side == SideEnum.Left)
         {
-            float rotation = transform.rotation.eulerAngles.z;
-            rigidBody.AddRelativeForce(new Vector3(0, (PlayerStats.RocketPower * 500 * forceMultiplier) - rotation, 0));
+            powerKey = KeyCode.LeftArrow;
+            if (rotation > 180 && rotation < 360 - maxRotation)
+                canAddMoreForce = false;
+        }
+        else if (side == SideEnum.Right)
+        {
+            powerKey = KeyCode.RightArrow;
+            if (rotation < 180 && maxRotation < rotation)
+                canAddMoreForce = false;
+        }
+
+        canAddMoreForce = true;
+        
+        if (Input.GetKey(powerKey) && canAddMoreForce)
+        {
+            float maxForce = (PlayerStats.RocketPower * 500 * forceMultiplier);
+
+            // calculating a multiplier that gets zero at the maxRotation and is one at zero rotation
+            // f(x) = - ax² + 1
+            bool rocketRotationToLeft = true;
+            float normalizedRotation = rotation;
+            if (normalizedRotation > 180)
+            {
+                rocketRotationToLeft = false;
+                normalizedRotation = 360 - normalizedRotation;
+            }
+            float a = 1 / (maxRotation * maxRotation);
+            float forceMultiplicator = -(a * Mathf.Pow(normalizedRotation, 2)) + 1;
+            if (forceMultiplicator < 0)
+                forceMultiplicator = 0;
+            float force = maxForce;
+
+            if(side == SideEnum.Right && rocketRotationToLeft ||
+                side == SideEnum.Left && !rocketRotationToLeft)
+            {
+                // damping of rocket force only when the rocket rotates to the opposite side of this engine
+                force *= forceMultiplicator;
+            }
+
+            GUIManager.SetTempDebugText(rotation.ToString());
+            rigidBody.AddRelativeForce(new Vector3(0, force, 0));
             forceMultiplier += 0.1f;
-            particleSystem.Play();
+            ParticleSystem.Play();
         }
         else
         {
