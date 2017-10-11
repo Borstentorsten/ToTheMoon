db321058b42f087c467ad3ec5fb1b6fb8387bb96
Borstentorsten <paule-mail@gmx.net>
2017-10-11 21:56:00 +0200

    Dampering of the rocket rotation

---
+++ ToTheMoon/Assets/GUIManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIManager : MonoBehaviour {
+
+    static Text tempDebugText;
+    void Start () {
+        tempDebugText = transform.FindChild("TempDebugText").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    static public void SetTempDebugText(string text)
+    {
+        tempDebugText.text = text;
+    }
+}
